0530cbc9633fe169518c7b395236f89e2db66e11
JorgeOrtega-UI <jorgeortega241005@gmail.com>
2025-08-05 15:10:56 -0500

    Add files via upload

---
+++ ProjectPhantom/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="assets/css/styles.css">
+    <title>ProjectPhantom - Home</title>
+</head>
+<body>
+    <div class="page-wrapper">
+        <div class="main-content">
+            <div class="general-content">
+                <div class="general-content-top">
+                    <div class="header">
+                        <div class="header-left"></div>
+                        <div class="header-right">
+                            <div class="header-item">
+                                <div class="header-button"></div>
+                            </div>
+                        </div>
+                        <div class="module-content module-options">
+                            <div class="menu-content">
+                                <div class="menu-list">
+                                    <div class="menu-link">
+                                        <div class="menu-link-icon"></div>
+                                        <div class="menu-link-text"></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="general-content-bottom">
+                    <div class="general-content-scrolleable"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
