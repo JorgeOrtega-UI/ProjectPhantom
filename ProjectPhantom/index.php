05b10c3aa7a7b2c97f300d15ac8f3f446c632467
JorgeOrtega-UI <jorgeortega241005@gmail.com>
2025-08-05 15:33:55 -0500

    Add files via upload

--- ProjectPhantom/index.php
+++ ProjectPhantom/index.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="assets/css/styles.css">
     <title>ProjectPhantom - Home</title>
 </head>
+
 <body>
     <div class="page-wrapper">
         <div class="main-content">
@@ -23,7 +25,9 @@
                                 <div class="menu-list">
                                     <div class="menu-link">
                                         <div class="menu-link-icon"></div>
-                                        <div class="menu-link-text"></div>
+                                        <div class="menu-link-text">
+                                            <span>Configuracion</span>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
@@ -33,15 +37,13 @@
                 <div class="general-content-bottom">
                     <div class="general-content-scrolleable">
                         <div class="section-wrapper">
-                            <div class="section-content active">
+                            <div class="section-content">
                                 <div class="section-hero">
                                     <h1>Every tool you need to work with PDFs in one place</h1>
                                     <h2>Every tool you need to use PDFs, at your fingertips. All are 100% FREE and easy to use! Merge, split, compress, convert, rotate, unlock and watermark PDFs with just a few clicks.</h2>
                                 </div>
+                                <div class="section-tools"></div>
                             </div>
-                            <div class="section-content disabled">2</div>
-                            <div class="section-content disabled">3</div>
-                            <div class="section-content disabled">4</div>
                         </div>
                     </div>
                 </div>
@@ -49,4 +51,5 @@
         </div>
     </div>
 </body>
+
 </html>
